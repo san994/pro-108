05208fa39655ae834bef5944fdb1cb912774fba3
san994 <75426313+san994@users.noreply.github.com>
2021-12-19 03:41:14 -0800

    Add files via upload

---
+++ ballCurve.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly.figure_factory as ff
+import csv
+
+df = pd.read_csv('pro 108/data.csv')
+
+fig = ff.create_distplot([df['Avg Rating'].tolist()],['rating'],show_hist=False)
+
+fig.show()
